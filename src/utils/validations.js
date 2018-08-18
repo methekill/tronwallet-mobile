3d0e8d663bd3786f031bdf7c05742e646c61525b
Renan Nascimento <Renan@costanascimento.com.br>
2018-08-17 21:46:35 -0300

    alias should be unique

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,8 +1,13 @@
-import { getAddressesFromStore } from './contactUtils'
+import { getAddressesFromStore, getAliasFromStore } from './contactUtils'
 
 export const isNameValid = (name) => name.match(/^[A-Za-z][ A-Za-z1-9]{0,32}$/)
 
 export const isAddressUnique = async (address) => {
   const addresses = await getAddressesFromStore()
   return !addresses.includes(address)
+}
+
+export const isAliasUnique = async (alias) => {
+  const aliasList = await getAliasFromStore()
+  return !aliasList.includes(alias)
 }
