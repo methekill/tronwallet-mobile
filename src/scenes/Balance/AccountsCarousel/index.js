6eb496469e8d439ddae7b7534fb056e31ecef52a
Carlos Navarrete <karnav@gmail.com>
2018-08-22 11:15:00 -0300

    Impr/multiple accounts card ui (#323)
    Fix design bug.

--- src/scenes/Balance/AccountsCarousel/index.js
+++ src/scenes/Balance/AccountsCarousel/index.js
@@ -20,6 +20,20 @@ import CardInfo from './CardInfo'
 const CURRENCIES = [tl.t('cancel'), 'USD', 'EUR', 'BTC', 'ETH']
 
 class AccountsCarousel extends React.Component {
+  shouldComponentUpdate (nextProps) {
+    const { accounts } = nextProps.context
+    const { accounts: oldAccounts } = this.props.context
+    for (let i = 0; i < accounts.length; i++) {
+      if (!oldAccounts[i]) return true
+      if (accounts[i].address !== oldAccounts[i].address) return true
+      if (accounts[i].name !== oldAccounts[i].name) return true
+      if (accounts[i].tronPower !== oldAccounts[i].tronPower) return true
+      if (accounts[i].bandwidth !== oldAccounts[i].bandwidth) return true
+      if (accounts[i].balance !== oldAccounts[i].balance) return true
+    }
+    return false
+  }
+
   // expose current index to parent
   currentIndex = () => this.carousel.currentIndex
 
@@ -87,7 +101,8 @@ class AccountsCarousel extends React.Component {
   }
 
   render () {
-    return !!this.props.accounts && (
+    const { accounts } = this.props.context
+    return !!accounts && (
       <React.Fragment>
         <ActionSheet
           ref={ref => { this.ActionSheet = ref }}
@@ -100,7 +115,7 @@ class AccountsCarousel extends React.Component {
           ref={ref => { this.carousel = ref }}
           layout='default'
           onSnapToItem={this.props.onSnapToItem}
-          data={this.props.accounts}
+          data={accounts}
           renderItem={this._renderItem}
           sliderWidth={Dimensions.get('window').width}
           itemWidth={300}
