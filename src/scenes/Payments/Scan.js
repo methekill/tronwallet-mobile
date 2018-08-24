a1fddf435219c3a14f939084faeb3278dd6db21e
Pedro Bertao <pedrobertao11@hotmail.com>
2018-08-24 09:44:05 -0300

    :ambulance: adding scan delay

--- src/scenes/Payments/Scan.js
+++ src/scenes/Payments/Scan.js
@@ -37,76 +37,77 @@ class ScanPayment extends Component {
 
   componentWillUnmount () {
     this._navListener.remove()
+    clearTimeout(this.scannerTimeout)
   }
   _checkAmount = (amount, token) => amount && (amount >= 1 / ONE_TRX)
 
   _checkDescription = description => description.length <= 500
 
-    _onRead = event => {
-      const { data } = event
-      const { navigation } = this.props
-      this.setState({loading: true})
-      try {
-        const parseData = JSON.parse(data)
-        const { address, amount, token, data: description } = parseData
-        if (!isAddressValid(address)) throw new DataError(tl.t('scanPayment.error.receiver'))
-        if (!token) throw new DataError(tl.t('scanPayment.error.token'))
-        if (!this._checkAmount(amount)) throw new DataError(tl.t('scanPayment.error.amount'))
-        if (description && !this._checkDescription(description)) throw new DataError(tl.t('scanPayment.error.description'))
+  _onRead = event => {
+    const { data } = event
+    const { navigation } = this.props
+    this.setState({loading: true})
+    try {
+      const parseData = JSON.parse(data)
+      const { address, amount, token, data: description } = parseData
+      if (!isAddressValid(address)) throw new DataError(tl.t('scanPayment.error.receiver'))
+      if (!token) throw new DataError(tl.t('scanPayment.error.token'))
+      if (!this._checkAmount(amount)) throw new DataError(tl.t('scanPayment.error.amount'))
+      if (description && !this._checkDescription(description)) throw new DataError(tl.t('scanPayment.error.description'))
 
-        this.setState({loading: false})
-        navigation.navigate('MakePayScene', {payment: {address, amount, token, description}})
-      } catch (error) {
-        if (error.name === 'DataError') Alert.alert(tl.t('warning'), error.message)
-        else Alert.alert(tl.t('warning'), tl.t('scanPayment.error.code'))
-        this.setState({loading: false})
-        this.scanner.reactivate()
-      }
+      this.setState({loading: false})
+      navigation.navigate('MakePayScene', {payment: {address, amount, token, description}})
+    } catch (error) {
+      if (error.name === 'DataError') Alert.alert(tl.t('warning'), error.message)
+      else Alert.alert(tl.t('warning'), tl.t('scanPayment.error.code'))
+      this.setState({loading: false})
+      this.scannerTimeout = setTimeout(() => this.scanner.reactivate(), 2000)
     }
+  }
 
-    render () {
-      const { navigation, loading } = this.props
-      return (
-        <Utils.Container>
-          <NavigationHeader
-            title={tl.t('scanPayment.scan')}
-            onBack={() => { navigation.goBack() }}
-            rightButton={loading ? <ActivityIndicator size='small' color={Colors.primaryText} /> : null}
-            noBorder
-          />
-          <QRCodeScanner
-            showMarker
-            fadeIn
-            ref={node => { this.scanner = node }}
-            customMarker={
+  render () {
+    const { navigation, loading } = this.props
+    return (
+      <Utils.Container>
+        <NavigationHeader
+          title={tl.t('scanPayment.scan')}
+          onBack={() => { navigation.goBack() }}
+          rightButton={loading ? <ActivityIndicator size='small' color={Colors.primaryText} /> : null}
+          noBorder
+        />
+        <QRCodeScanner
+          showMarker
+          fadeIn
+          ref={node => { this.scanner = node }}
+          customMarker={
+            <Utils.View
+              flex={1}
+              background='transparent'
+              justify='center'
+              align='center'
+            >
               <Utils.View
-                flex={1}
-                background='transparent'
-                justify='center'
-                align='center'
-              >
-                <Utils.View
-                  width={250}
-                  height={250}
-                  borderWidth={2}
-                  borderColor={'white'}
-                />
-                <Utils.Text marginTop='medium' align='center'>
-                  {tl.t('components.QRScanner.explanation')}
-                </Utils.Text>
-              </Utils.View>
-            }
-            cameraStyle={{
-              height: '100%',
-              width: '100%',
-              justifyContent: 'flex-start'
-            }}
-            permissionDialogMessage={tl.t('components.QRScanner.permissionMessage')}
-            onRead={this._onRead}
-          />
-        </Utils.Container>
-      )
-    }
+                width={250}
+                height={250}
+                borderWidth={2}
+                borderColor={'white'}
+              />
+              <Utils.Text marginTop='medium' align='center'>
+                {tl.t('components.QRScanner.explanation')}
+              </Utils.Text>
+            </Utils.View>
+          }
+          cameraStyle={{
+            height: '100%',
+            width: '100%',
+            justifyContent: 'flex-start'
+          }}
+          permissionDialogMessage={tl.t('components.QRScanner.permissionMessage')}
+          onRead={this._onRead}
+        />
+      </Utils.Container>
+    )
+  }
 }
 
 export default withContext(ScanPayment)
--- src/utils/assetsUtils.js
+++ src/utils/assetsUtils.js
@@ -1,7 +1,7 @@
 import Client from '../services/client'
 import getAssetsStore from '../store/assets'
 
-export const updateAssets = async (start = 0, limit = 100, name = '') => {
+export const updateAssets = async (start = 0, limit = 100, name = 'twx') => {
   const assets = await Client.getTokenList(start, limit, name)
   const store = await getAssetsStore()
 
