25a9802277ba0b036717cb0c788a8f3a73595ae1
Renan Nascimento <renan@costanascimento.com.br>
2018-08-30 11:31:19 -0300

    replaced es.js with es.json

--- src/utils/translations/index.js
+++ src/utils/translations/index.js
@@ -1,10 +1,10 @@
-import es from './es'
 import ch from './ch'
 
 const en = require('./en.json')
 const pt = require('./pt.json')
 const fr = require('./fr.json')
 const nl = require('./nl.json')
+const es = require('./es.json')
 
 export default {
   en,
