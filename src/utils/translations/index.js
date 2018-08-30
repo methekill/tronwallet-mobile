0f7ed50c653f6d31e19f374f67e9b45dd09bf2d9
Renan Nascimento <renan@costanascimento.com.br>
2018-08-30 11:39:35 -0300

    replaced ch.js with ch.json

--- src/utils/translations/index.js
+++ src/utils/translations/index.js
@@ -1,10 +1,9 @@
-import ch from './ch'
-
 const en = require('./en.json')
 const pt = require('./pt.json')
 const fr = require('./fr.json')
 const nl = require('./nl.json')
 const es = require('./es.json')
+const ch = require('./ch.json')
 
 export default {
   en,
