ddc7789c85b765c16b939db2d7fc597e5b6ac578
Renan Nascimento <renan@costanascimento.com.br>
2018-08-30 10:53:42 -0300

    replaced br.js with pt.json

--- src/utils/translations/index.js
+++ src/utils/translations/index.js
@@ -1,14 +1,14 @@
-import br from './br'
 import fr from './fr'
 import nl from './nl'
 import es from './es'
 import ch from './ch'
 
 const en = require('./en.json')
+const pt = require('./pt.json')
 
 export default {
   en,
-  pt: br,
+  pt,
   fr,
   nl,
   es,
