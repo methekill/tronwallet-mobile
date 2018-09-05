31536d266d1c44ee00e6b08b6059dd6b8f2800b1
Pedro Bertao <pedrobertao11@hotmail.com>
2018-09-05 10:37:29 -0300

    :pencil2: fixing name

--- src/utils/navigationUtils.js
+++ src/utils/navigationUtils.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native'
 export const replaceRoute = (navigation, routeTo, params) => {
-  const transationTime = Platform.OS === 'android' ? 550 : 850
+  const transitionTime = Platform.OS === 'android' ? 550 : 850
   navigation.goBack(null)
-  setTimeout(() => navigation.navigate(routeTo, params), transationTime)
+  setTimeout(() => navigation.navigate(routeTo, params), transitionTime)
 }
