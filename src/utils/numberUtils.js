996a6c5d42b9a023909692d00688051f42c3dc8a
Jordan Cassiano <jordan_gnr@hotmail.com>
2018-11-07 13:44:19 -0200

    update shortNumberFormat

--- src/utils/numberUtils.js
+++ src/utils/numberUtils.js
@@ -38,18 +38,15 @@ export const shortNumberFormat = (number) => {
   }
 
   if (number < 1000000) {
-    return Math.round(number / 1000) + 'K'
-  }
-  if (number < 10000000) {
-    return (number / 1000000).toFixed(2) + 'M'
+    return (number / 1000).toFixed(1) + 'K'
   }
 
   if (number < 1000000000) {
-    return Math.round((number / 1000000)) + 'M'
+    return (number / 1000000).toFixed(2) + 'M'
   }
 
   if (number < 1000000000000) {
-    return Math.round((number / 1000000000)) + 'B'
+    return Math.floor((number / 1000000000)) + 'B'
   }
 
   return '1T+'
