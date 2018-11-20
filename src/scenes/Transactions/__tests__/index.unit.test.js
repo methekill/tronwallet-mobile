ce2ff3952f6839b1ce191bd1b4f35a98ab4e329a
Calebe Santos <calebe.bsantos@outlook.com>
2018-11-20 16:39:13 -0200

    extract load transaction scene component beforeAll

--- src/scenes/Transactions/__tests__/index.unit.test.js
+++ src/scenes/Transactions/__tests__/index.unit.test.js
@@ -11,24 +11,31 @@ jest.mock('react-native-mixpanel')
 jest.mock('../../../utils/i18n')
 
 describe('Transaction Scene', () => {
-  test('Should add listener when componentDidMount called', () => {
-    const addListenerMockResult = jest.fn()
+  let transactionScene = null
+  let addListenerMockResult = null
+
+  beforeAll(() => {
+    addListenerMockResult = jest.fn()
     const addListenerMock = jest.fn(() => (addListenerMockResult))
     const props = { navigation: { addListener: addListenerMock } }
 
-    const { wrapper } = setupTransactionScene(props)
+    transactionScene = setupTransactionScene(props)
+  })
+
+  test('Should add listener when componentDidMount called', () => {
+    const { wrapper } = transactionScene
     const instance = wrapper.instance()
 
     instance.componentDidMount()
+
     expect(Answers.logContentView).toBeCalledWith('Tab', 'Transactions')
-    expect(addListenerMock).toBeCalledWith('didFocus', instance._didFocus)
     expect(instance._didFocusSubscription).toEqual(addListenerMockResult)
   })
 
   test('Should remove listener when componentWillUnmount called', () => {
     const removeSubscription = jest.fn()
 
-    const { wrapper } = setupTransactionScene()
+    const { wrapper } = transactionScene
     const instance = wrapper.instance()
     instance._didFocusSubscription = { remove: removeSubscription }
 
@@ -38,15 +45,15 @@ describe('Transaction Scene', () => {
   })
 
   test('Should refresh the data when clicking on the refresh Button', () => {
-    const { wrapper, syncButton } = setupTransactionScene()
+    const { wrapper, syncButton } = transactionScene
     const spy = jest.spyOn(wrapper.instance(), '_loadData')
 
     syncButton.props.onPress()
     expect(spy).toBeCalled()
   })
 
-  test('Should load expected transactions when _setData called', async () => {
-    const { wrapper } = setupTransactionScene()
+  test('Should load expected transactions when data is loaded', async () => {
+    const { wrapper } = transactionScene
     const instance = wrapper.instance()
 
     const spy = jest.spyOn(instance, '_loadData')
