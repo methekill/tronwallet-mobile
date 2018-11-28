a2a03db791af98fa557ebc4c83f901faff514f0b
Bertao <pedrobertao11@hotmail.com>
2018-11-28 12:07:48 -0200

    fixing testing

--- src/scenes/Transactions/__tests__/snapshots.test.js
+++ src/scenes/Transactions/__tests__/snapshots.test.js
@@ -2,6 +2,7 @@ import transactionListMock from '../../../services/__mocks__/transactionList'
 import { setupTransactionScene } from './Utils'
 
 jest.mock('../../../utils/i18n')
+jest.mock('../../../services/client')
 
 describe('Transaction snapshots test', () => {
   test('Should Transactions/index match snapshot', () => {
