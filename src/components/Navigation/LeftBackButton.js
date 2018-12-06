b08321a31110aea7ef8dad78a09faaeeb6ce3e04
Calebe Santos <calebe.bsantos@outlook.com>
2018-12-06 20:33:02 -0200

    improve code due review

--- src/components/Navigation/LeftBackButton.js
+++ src/components/Navigation/LeftBackButton.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { func } from 'prop-types'
+import { TouchableOpacity } from 'react-native'
 
 import { Colors } from '../DesignSystem'
-import { LeftBackButtonTouchable } from './styles'
 
 const LeftBackButton = ({ onBack }) => (
-  <LeftBackButtonTouchable onPress={onBack} testID='HeaderBack'>
+  <TouchableOpacity style={{padding: 8, position: 'absolute', left: 20, top: 20}} onPress={onBack} testID='HeaderBack'>
     <Ionicons
       name='ios-arrow-round-back'
       size={36}
       color={Colors.primaryText}
     />
-  </LeftBackButtonTouchable>
+  </TouchableOpacity>
 )
 
 LeftBackButton.propTypes = {
