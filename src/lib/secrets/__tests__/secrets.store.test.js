e775498044db976c2dea7e2560f3935df7dd33c6
Calebe Santos <calebe.bsantos@outlook.com>
2018-12-07 13:29:48 -0200

    create user actions

--- src/lib/secrets/__tests__/secrets.store.test.js
+++ src/lib/secrets/__tests__/secrets.store.test.js
@@ -2,46 +2,133 @@ import RealmStoreMock from '../../infra-data/__mocks__/realm.store.mock'
 import SecretsStore from '../secrets.store'
 
 describe('Secret store', () => {
-  let secretStore, store, mockData
+  let _secretStore, _store, _mockData, _RNTron
 
   beforeEach(() => {
-    mockData = [
+    _mockData = [
       { key: 1, value: 'a', hide: false },
       { key: 2, value: 'b', hide: true }
     ]
 
-    store = new RealmStoreMock('key', mockData)
+    _RNTron = {
+      generateKeypair: () => ({})
+    }
 
-    secretStore = new SecretsStore(store)
+    _store = new RealmStoreMock('key', _mockData)
+    _secretStore = new SecretsStore(_store, _RNTron)
   })
 
-  test('should return all accounts when accounts list exist', () => {
-    const accounts = secretStore.findAllAccounts()
+  describe('Create user secret account', () => {
+    test('should create when account name and mnemonic was filled', async () => {
+      const accountName = 'Asdf 1234'
+      const mnemonic = 'asdf qwer zxcv'
 
-    expect(accounts).toMatchObject(mockData)
+      const newUser = await _secretStore.create(accountName, mnemonic)
+
+      const expected = {
+        mnemonic,
+        name: accountName,
+        alias: '@asdf_1234',
+        confirmed: true,
+        hide: false
+      }
+
+      expect(newUser).toEqual(expected)
+    })
+
+    test('should not create when account name wasn\'t filled', async () => {
+      const accountName = ''
+      const mnemonic = 'asdf qwer zxcv'
+
+      const newUser = await _secretStore.create(accountName, mnemonic)
+
+      expect(newUser).toBeNull()
+    })
+
+    test('should not create when mnemonic wasn\'t filled', async () => {
+      const accountName = 'Asdf 1234'
+      const mnemonic = ''
+
+      const newUser = await _secretStore.create(accountName, mnemonic)
+
+      expect(newUser).toBeNull()
+    })
   })
 
-  test('should return empty list when accounts list is empty', () => {
-    const store = new RealmStoreMock('key', [])
-    const secretStore = new SecretsStore(store)
+  describe('Create FIRST user secret account', () => {
+    test('should create when mnemonic filled and accounts is empty', async () => {
+      const store = new RealmStoreMock('key', [])
+      const secretStore = new SecretsStore(store, _RNTron)
 
-    const accounts = secretStore.findAllAccounts()
+      const mnemonic = 'asdf qwer zxcv'
 
-    expect(accounts).toHaveLength(0)
+      const newUser = await secretStore.createFirstAccount(mnemonic)
+
+      const expected = {
+        mnemonic,
+        name: 'Main account',
+        alias: '@main_account',
+        confirmed: true,
+        hide: false
+      }
+
+      expect(newUser).toEqual(expected)
+    })
+
+    test('should not create when mnemonic wasn\'t filled and accounts isn\'t empty', async () => {
+      const mnemonic = ''
+
+      const newUser = await _secretStore.createFirstAccount(mnemonic)
+
+      expect(newUser).toBeNull()
+    })
+
+    test('should not create when mnemonic wasn\'t filled', async () => {
+      const mnemonic = ''
+
+      const newUser = await _secretStore.createFirstAccount(mnemonic)
+
+      expect(newUser).toBeNull()
+    })
+
+    test('should not create when accounts isn\'t empty', async () => {
+      const mnemonic = 'asdf qwer zxcv'
+
+      const newUser = await _secretStore.createFirstAccount(mnemonic)
+
+      expect(newUser).toBeNull()
+    })
   })
 
-  test('should return only visible accounts when list have visible accounts', () => {
-    const accounts = secretStore.findVisibleAccounts()
+  describe('List accounts', () => {
+    test('should return all accounts when accounts list exist', () => {
+      const accounts = _secretStore.findAllAccounts()
 
-    expect(accounts).toMatchObject([{ key: 1, value: 'a', hide: false }])
-  })
+      expect(accounts).toMatchObject(_mockData)
+    })
 
-  test('should return empty list when list doesn\'t have visible accounts', () => {
-    const store = new RealmStoreMock('key', [{ key: 1, value: 'a', hide: true }])
-    const secretStore = new SecretsStore(store)
+    test('should return empty list when accounts list is empty', () => {
+      const store = new RealmStoreMock('key', [])
+      const secretStore = new SecretsStore(store)
 
-    const accounts = secretStore.findVisibleAccounts()
+      const accounts = secretStore.findAllAccounts()
 
-    expect(accounts).toHaveLength(0)
+      expect(accounts).toHaveLength(0)
+    })
+
+    test('should return only visible accounts when list have visible accounts', () => {
+      const accounts = _secretStore.findVisibleAccounts()
+
+      expect(accounts).toEqual([{ key: 1, value: 'a', hide: false }])
+    })
+
+    test('should return empty list when list doesn\'t have visible accounts', () => {
+      const store = new RealmStoreMock('key', [{ key: 1, value: 'a', hide: true }])
+      const secretStore = new SecretsStore(store)
+
+      const accounts = secretStore.findVisibleAccounts()
+
+      expect(accounts).toHaveLength(0)
+    })
   })
 })
--- src/lib/secrets/secrets.store.js
+++ src/lib/secrets/secrets.store.js
@@ -1,6 +1,7 @@
 class SecretsStore {
-  constructor (store) {
+  constructor (store, tron) {
     this._store = store
+    this._tron = tron
   }
 
   createMnemonic () {}
@@ -11,8 +12,60 @@ class SecretsStore {
 
   signTransaction () {}
 
+  async create (accountName, mnemonic) {
+    if (!accountName || !mnemonic) {
+      return null
+    }
+
+    const accounts = this.findAllAccounts()
+
+    const userSecret = await this._tron.generateKeypair(mnemonic, accounts.length, false)
+
+    userSecret.mnemonic = mnemonic
+    userSecret.name = accountName
+    userSecret.alias = this.formatAlias(accountName)
+    userSecret.confirmed = true
+    userSecret.hide = false
+
+    this._store.save(userSecret)
+
+    return userSecret
+  }
+
+  async createFirstAccount (mnemonic) {
+    const accounts = this.findAllAccounts()
+    if (accounts.length) {
+      return null
+    }
+
+    return this.create('Main account', mnemonic)
+  }
+
+  async createByExistentAccount (accountName) {
+    const firstAccount = this.findFirstAccount()
+    if (!firstAccount) {
+      return null
+    }
+
+    return this.create(accountName, firstAccount.mnemonic)
+  }
+
+  formatAlias (name) {
+    return name && `@${name.trim().toLowerCase().replace(/ /g, '_')}`
+  }
+
   findAllAccounts () {
-    return this._store.findAll()
+    return this._store.findAll() || []
+  }
+
+  findFirstAccount () {
+    const accounts = this.findAllAccounts()
+    return accounts[0]
+  }
+
+  findLastAccount () {
+    const accounts = this.findAllAccounts()
+    return accounts[accounts.length - 1]
   }
 
   findVisibleAccounts () {
@@ -20,7 +73,7 @@ class SecretsStore {
   }
 
   get publicKey () {
-    const account = this.findAllAccounts()[0]
+    const account = this.findFirstAccount()
     return (account || {}).address
   }
 }
