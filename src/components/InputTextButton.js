c791b13c928f90826f7407a7709e16f4512a4254
Bertao <pedrobertao11@hotmail.com>
2018-12-26 17:35:26 -0200

    adding feedbacks

--- src/components/InputTextButton.js
+++ src/components/InputTextButton.js
@@ -8,11 +8,11 @@ const ButtonWrapper = styled.TouchableOpacity`
   height: 36px;
   width: 36px;
   border-radius: 4px;
-  background-color: #2E2F47;
+  background-color: ${Colors.darkThree};
   align-items: center;
   justify-content: center;
   elevation: 2;
-  shadow-color: #000000;
+  shadow-color: ${Colors.shadow};
   shadow-offset: 0 2px;
   shadow-radius: 4px;
   shadow-opacity: 0.5;
