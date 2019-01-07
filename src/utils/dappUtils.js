cfe63852114eb7f22396c98ace968f82d58983b8
Marlon Gomes <marlonbrgomes@gmail.com>
2019-01-07 15:38:22 -0200

    update dappUtils to crud functions

--- src/utils/dappUtils.js
+++ src/utils/dappUtils.js
@@ -1,5 +1,7 @@
 // import { AsyncStorage } from 'react-native'
 import * as Async from './asyncStorageUtils'
+import { AsyncStorage } from 'react-native'
+
 import { DAPPS_SEARCH_HISTORY } from './constants'
 
 // AsyncStorage.removeItem(DAPPS_SEARCH_HISTORY)
@@ -15,4 +17,27 @@ export function saveSearchHistory (value) {
 
 export function getSearchHistory () {
   return Async.get(DAPPS_SEARCH_HISTORY, '[]').then(data => JSON.parse(data))
+}
+
+export const addToList = async (name, data) => {
+  const list = JSON.parse(await AsyncStorage.getItem(name))
+
+  list.data.push(data)
+  list.data.reverse()
+  list.data = list.data.slice(0, list.limit)
+  list.data.reverse()
+
+  return AsyncStorage.setItem(name, JSON.stringify(list))
+}
+
+export const readList = async (listName) => {
+  return JSON.parse(await AsyncStorage.getItem(listName)).data
+}
+
+export const createList = ({ name, limit }) => {
+  return AsyncStorage.setItem(name, JSON.stringify({ limit, data: [] }))
+}
+
+export const deleteList = async (listName) => {
+  return AsyncStorage.removeItem(listName)
 }
